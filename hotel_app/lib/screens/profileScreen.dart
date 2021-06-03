ce35eddc5558a0e63816dbb8342e44bd1e08a487
Phạm Anh Cường <phamanhcuongvna2511@gmail.com>
2021-06-04 00:33:24 +0700

    Demo profile screen v2

--- hotel_app/lib/screens/profileScreen.dart
+++ hotel_app/lib/screens/profileScreen.dart
@@ -12,6 +12,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: Text('Profile'),
       ),
+      
       body: Column(
         children: <Widget>[
           Container(
@@ -19,9 +20,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
               gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
-                colors: [Colors.blue, Colors.lightBlue]
+                colors: [Colors.white, Colors.white]
               )
             ),
+
             child: Container(
               width: double.infinity,
               height: 250.0,
@@ -32,126 +34,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   children: <Widget>[
                     CircleAvatar(
                       radius: 50.0,
+                      backgroundColor: Colors.orange,
                     ),
-                    SizedBox(
-                      height: 5.0,
+
+                    SizedBox (
+                      height: 10.0,
                     ),
-                    Text(
-                      "Cuong", // Lấy API đi 
+
+                    Text( // Lấy API đi 
+                      "Cuong", 
                       style: TextStyle(
+                        fontWeight: FontWeight.bold,
                         fontSize: 20.0,
-                        color: Colors.white,
+                        color: Colors.black,
                       ),
                     ),
+
                     SizedBox(
                       height: 5.0,
                     ),
+
                     Text(
                       "pacman@gmail.com", // Lấy API đi 
                       style: TextStyle(
                         fontSize: 20.0,
-                        color: Colors.white,
+                        color: Colors.black,
                       ),
                     )
-                    /*SizedBox(
-                      height: 10.0,
-                    ),*/
-                    /*Card(
-                      margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
-                      clipBehavior: Clip.antiAlias,
-                      color: Colors.white,
-                      elevation: 5.0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 22.0),
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
-
-                                children: <Widget>[
-                                  Text(
-                                    "Posts",
-                                    style: TextStyle(
-                                      color: Colors.redAccent,
-                                      fontSize: 22.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5.0,
-                                  ),
-                                  Text(
-                                    "5200",
-                                    style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.pinkAccent,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              child: Column(
-
-                                children: <Widget>[
-                                  Text(
-                                    "Followers",
-                                    style: TextStyle(
-                                      color: Colors.redAccent,
-                                      fontSize: 22.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5.0,
-                                  ),
-                                  Text(
-                                    "28.5K",
-                                    style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.pinkAccent,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              child: Column(
-
-                                children: <Widget>[
-                                  Text(
-                                    "Follow",
-                                    style: TextStyle(
-                                      color: Colors.redAccent,
-                                      fontSize: 22.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5.0,
-                                  ),
-                                  Text(
-                                    "1300",
-                                    style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.pinkAccent,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    )*/
                   ],
                 ),
               ),
             )
           ),
+
           SizedBox(
             height: 20.0,
           ),
+
           Container(
             width: 200.00,
           ),
