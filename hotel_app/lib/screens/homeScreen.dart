230bc46c5bf49aa69071b84e272853f32ddb2d78
Nguyễn Quang Đại Dương <19020265@vnu.edu.vn>
2021-06-10 14:52:25 +0700

    Add favourite screen

--- hotel_app/lib/screens/homeScreen.dart
+++ hotel_app/lib/screens/homeScreen.dart
@@ -77,7 +77,7 @@ class _HomeScreenState extends State<HomeScreen> {
               size: 30.0,
             ),
             title: Text(
-              'Sự kiện',
+              'Khuyến mãi',
               style: bottomTextStyle,
             ),
           ),
